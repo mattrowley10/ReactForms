ebc166d51b47250284b53c52d0bb71fb90110b92
Matthew Rowley <mattrowley10@gmail.com>
2023-04-05 09:44:06 -0600

    Workshop Complete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
 const Form = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(password)
+    console.log(username)
+    setUsername('')
+    setPassword('')
+  }
+  const handleChange = (event) => {
+    setUsername(event.target.value)
+  }
   return (
     <div id='container'>
       <div id='navbar'>
         Form.js
       </div>
-      <form>
-        {/* your form fields here */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username:</label>
+        <input type='text' name='username' value={username} onChange={handleChange} />
+        <label htmlFor='password'>Password:</label>
+        <input type='text' name='password' value={password} onChange={(event) => setPassword(event.target.value)}/>
+        <button type='submit'>Submit</button>
       </form>
     </div>
   )
